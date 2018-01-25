fbb32735a472926d58b6fa629cff6acd800571d4
CarlOlson <CarlOlson@users.noreply.github.com>
2018-01-24 19:32:32 -0600

    add shell spike

--- shell/shell.py
+++ shell/shell.py
@@ -1,25 +1,51 @@
 
 from subprocess import Popen, PIPE
 import sys
-import _thread
+import threading
+import os
+import hashlib
+import json
+
+BLOCKCHAIN_FILE = 'blockchain-server.txt'
+
+
+class Block(dict):
+    def __init__(self, previous, data):
+        dict.__init__(self, data=data)
+        self['previous_hash'] = '' if previous is None else previous['hash']
+        self['hash'] = self.hash()
+
+    def hash(self):
+        hash_data = self['data'] + self['previous_hash']
+        hash_data = str.encode(hash_data)
+        return hashlib.sha256(hash_data).hexdigest()
 
 def main():
-    p = Popen("/bin/sh", stdin=PIPE, stdout=PIPE)
+    os.chdir('/Users/testuser')
+
+    blockchain = open(BLOCKCHAIN_FILE, 'w')
+    currentBlock = None
+
+    p = Popen("/bin/bash", stdin=PIPE, stdout=PIPE)
     stdin, stdout = p.stdin, p.stdout
 
     def watch_output(stdout_handle):
-        try:
-            while True:
-                sys.stdout.write(stdout_handle.read(1))
-                sys.stdout.flush()
-        except Exception:
-            pass
+        while True:
+            sys.stdout.write(stdout_handle.read(1).decode())
+            sys.stdout.flush()
 
-    _thread.start_new_thread(watch_output, (stdout,))
+    t = threading.Thread(target=watch_output, args=(stdout,))
+    t.start()
 
     while True:
         command_input = str.encode(sys.stdin.readline())
         stdin.write(command_input)
+        stdin.flush()
+
+        command_input = command_input.decode()
+        currentBlock = Block(currentBlock, command_input)
+        blockchain.write('{} {}'.format(json.dumps(currentBlock), command_input))
+        blockchain.flush()
 
 if __name__ == '__main__':
     main()
